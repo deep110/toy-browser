75e17bb612787dac67d7aeb430135f0f6b144b28
Deepankar Agrawal <deepankar11093@gmail.com>
2020-04-19 00:27:18 +0530

    add glutin for getting opengl context

--- src/window.rs
+++ src/window.rs
@@ -1,29 +1,42 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
 
 pub fn open_browser(title: &str) {
-    let event_loop = EventLoop::new();
-    let window = create_window(title).build(&event_loop).unwrap();
+    let el = EventLoop::new();
+    let wb = create_window(title);
 
-    event_loop.run(move |event, _, control_flow| {
+    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+    println!(
+        "Pixel format of the window's GL context: {:?}",
+        windowed_context.get_pixel_format()
+    );
+
+    // let gl = support::load(&windowed_context.context());
+
+    el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            Event::MainEventsCleared => {
-                window.request_redraw();
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                // gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+                windowed_context.swap_buffers().unwrap();
             }
             _ => (),
         }
     });
 }
 
-fn create_window(title : &str) -> WindowBuilder {
+fn create_window(title: &str) -> WindowBuilder {
     WindowBuilder::new().with_resizable(true).with_title(title)
 }
